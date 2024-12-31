1c18841cdc488dc53beabb2cc35f97b8e1d6fc7b
danmoldoveanu <dan.moldoveanu@admintools.io>
2024-12-31 16:43:35 +0200

    WIP: login form

--- tua/src/routes/admin/login.tsx
+++ tua/src/routes/admin/login.tsx
@@ -1,7 +1,12 @@
-import { createForm, required, minLength, value } from "@modular-forms/solid";
+import {
+  createForm,
+  required,
+  minLength,
+  SubmitHandler,
+} from "@modular-forms/solid";
 
 import { ImageRoot, ImageFallback, Image } from "@/components/ui/image";
-import { Skeleton } from "@/components/ui/skeleton";
+
 import {
   TextField,
   TextFieldLabel,
@@ -9,6 +14,9 @@ import {
   TextFieldErrorMessage,
 } from "@/components/ui/textfield";
 import { Button } from "@/components/ui/button";
+import { FaRegularEyeSlash } from "solid-icons/fa";
+import { FaRegularEye } from "solid-icons/fa";
+import { createSignal } from "solid-js";
 
 type LoginForm = {
   username: string;
@@ -16,14 +24,38 @@ type LoginForm = {
 };
 
 export default function Login() {
+  const [showPassword, setShowPassword] = createSignal(false);
+
   const [loginForm, { Form, Field }] = createForm<LoginForm>();
+
+  let passwordInputRef: HTMLInputElement;
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+
+    if (passwordInputRef) {
+      passwordInputRef.focus();
+    }
+  };
+
+  const handleSubmit: SubmitHandler<LoginForm> = (values, event) => {
+    event.preventDefault();
+
+    console.debug({ values });
+  };
+
   return (
     <main class="h-svh w-svw flex">
-      <Form class="h-[64dvh] max-h-[512px] w-[300px] m-auto flex flex-col justify-between">
+      <Form
+        class="h-[64dvh] max-h-[512px] w-[300px] m-auto flex flex-col justify-between"
+        onSubmit={handleSubmit}
+      >
         <ImageRoot>
           <Image src="/tua.webp" class="w-[300px]" alt="TUA" />
-          <ImageFallback class="h-[128px] w-[300px] !rounded-none">
-            <Skeleton class="bg-[#7f805d] h-full w-full" />
+          <ImageFallback class="h-[128px] w-[300px] !rounded-none !bg-transparent">
+            <div class="text-[#7f805d] text-[72px] text-center h-full w-full">
+              TUA
+            </div>
           </ImageFallback>
         </ImageRoot>
         <Field
@@ -37,14 +69,13 @@ export default function Login() {
             <TextFieldRoot
               class="w-full max-w-xs !h-[64px]"
               validationState={field.error ? "invalid" : "valid"}
+              value={field.value || ""}
             >
               <TextField
                 {...props}
+                class="h-[48px]"
                 type="text"
                 placeholder="Utilizator"
-                on:input={(e) => {
-                  console.debug(e.target.value);
-                }}
               />
               <TextFieldErrorMessage>
                 {field.error ?? " "}
@@ -60,22 +91,37 @@ export default function Login() {
           ]}
         >
           {(field, props) => (
-            <TextFieldRoot
-              class="w-full max-w-xs !h-[64px]"
-              validationState={field.error ? "invalid" : "valid"}
-            >
-              <TextField
-                {...props}
-                type="text"
-                placeholder="Parolă"
-                on:input={(e) => {
-                  console.debug(e.target.value);
-                }}
-              />
-              <TextFieldErrorMessage forceMount={true} class="text-opacity-0">
-                {field.error}
-              </TextFieldErrorMessage>
-            </TextFieldRoot>
+            <div class="relative flex w-full max-w-xs !h-[72px]">
+              <TextFieldRoot
+                class="w-full max-w-xs !h-[72px] absolute bottom-0 left-0 "
+                validationState={field.error ? "invalid" : "valid"}
+                value={field.value || ""}
+              >
+                <TextField
+                  {...props}
+                  class="h-[48px]"
+                  type={showPassword() ? "text" : "password"}
+                  placeholder="Parolă"
+                  ref={(element: HTMLInputElement) =>
+                    (passwordInputRef = element)
+                  }
+                />
+                <TextFieldErrorMessage forceMount={true} class="text-opacity-0">
+                  {field.error}
+                </TextFieldErrorMessage>
+              </TextFieldRoot>
+              <Button
+                type="button"
+                class="rounded-full bg-transparent p-2 hover:bg-white hover:bg-opacity-15 absolute right-[6px] top-[6px]"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword() ? (
+                  <FaRegularEyeSlash color="white" size={20} />
+                ) : (
+                  <FaRegularEye color="white" size={20} />
+                )}
+              </Button>
+            </div>
           )}
         </Field>
         <Button

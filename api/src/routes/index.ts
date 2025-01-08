0331132889fa20966d69983799e8db0ce53307bc
danmoldoveanu <dan.moldoveanu@admintools.io>
2025-01-08 18:43:21 +0200

    WIP: backend refactoring

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,7 @@
-var express = require("express");
-var router = express.Router();
+import express from "express";
+import { createSession } from "better-sse";
 
-const { createSession } = require("better-sse");
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
@@ -16,4 +16,4 @@ router.get("/", async function (req, res, next) {
   }, 1000);
 });
 
-module.exports = router;
+export default router;
